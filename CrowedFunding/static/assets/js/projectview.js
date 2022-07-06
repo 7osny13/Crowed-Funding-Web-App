e5564ae8229feda62fb6b3c59e9196597eeb9dcd
Your Name <you@example.com>
2022-07-06 15:46:20 +0200

    add donation project and enhance pages

--- CrowedFunding/static/assets/js/projectview.js
+++ CrowedFunding/static/assets/js/projectview.js
@@ -1,7 +1,16 @@
-function changeImage(element) {
+// function changeImage(element) {
 
-    var main_prodcut_image = document.getElementById('main_product_image');
-    main_prodcut_image.src = element.src;
+//     var main_prodcut_image = document.getElementById('main_product_image');
+//     main_prodcut_image.src = element.src;
     
 
-}
+// }
+
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
